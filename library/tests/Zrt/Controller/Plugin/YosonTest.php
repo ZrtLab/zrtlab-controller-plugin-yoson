f04b775e8bcf0b8ca2460170fb7ebb45a88c1b25
Luis Alberto Mayta Mamani <slovacus@gmail.com>
2014-05-26 20:31:36 -0500

    added class test Yoson

---
+++ library/tests/Zrt/Controller/Plugin/YosonTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Zrt\Tests\Controller\Plugin;
+
+use Zrt_Controller_Plugin_Yoson,
+    PHPUnit_Framework_TestCase;
+
+class YosonTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+
+    }
+}
